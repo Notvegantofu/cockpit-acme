6358ec75346c3cf5776b1acb8840b6079d801c07
Tobias Vale <tobias.vale@reguest.io>
2025-03-12 08:56:54 +0100

    Added the GUI

--- src/app.tsx
+++ src/app.tsx
@@ -18,34 +18,29 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
-import { BackToTop } from '@patternfly/react-core';
-import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import { HorizontalNav } from './components/HorizontalNav';
+import { DomainTable } from './components/DomainTable';
+import { AddDomainForm } from './components/AddDomainForm'
+import { Divider, Stack, StackItem } from '@patternfly/react-core';
 
 import cockpit from 'cockpit';
 
 const _ = cockpit.gettext;
 
-export const Application = () => {
-    const [hostname, setHostname] = useState(_("Unknown"));
 
-    useEffect(() => {
-        const hostname = cockpit.file('/etc/hostname');
-        hostname.watch(content => setHostname(content?.trim() ?? ""));
-        return hostname.close;
-    }, []);
+export const Application = () => {
+    const [content, setContent] = useState(0);
+
+    const actions= [
+      <StackItem><DomainTable/></StackItem>,
+      <StackItem><AddDomainForm/></StackItem>
+    ]
 
     return (
-        <>
-            <Card>
-                <CardTitle>{"Acme"}</CardTitle>
-                <CardBody>
-                    <Alert
-                        variant="info"
-                        title={ cockpit.format(_("Running on $0"), hostname) }
-                    />
-                </CardBody>
-            </Card>
-        </>
+        <Stack id="mainStack">
+            <StackItem><HorizontalNav setAction={setContent}/></StackItem>
+            <StackItem><Divider/></StackItem>
+            {actions[content]}
+        </Stack>
     );
 };
