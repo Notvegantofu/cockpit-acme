7ae1a2478c7d5bd7d03227b4c0f68d68f6ed1e0a
Tobias Vale <tobias.vale@reguest.io>
2025-03-12 14:34:07 +0100

    Added DevMode for addDomain testing

--- src/components/AddDomainForm.tsx
+++ src/components/AddDomainForm.tsx
@@ -14,18 +14,18 @@ import cockpit from 'cockpit';
 export const AddDomainForm: React.FunctionComponent = () => {
   const [mainDomain, setMainDomain] = React.useState('');
   const [sanDomains, setSanDomains] = React.useState('');
+  const commandBegin = ["sudo", "-u", "acme", "/usr/local/bin/acme.sh"];
+  const devMode = true;
 
-  const handleMainDomainChange = (_event: React.FormEvent<HTMLInputElement>, mainDomain: string) => {
+  function handleMainDomainChange(_event: React.FormEvent<HTMLInputElement>, mainDomain: string) {
     setMainDomain(mainDomain);
   };
 
-  const handleSanDomainsChange = (_event: React.FormEvent<HTMLInputElement>, sanDomains: string) => {
+  function handleSanDomainsChange(_event: React.FormEvent<HTMLInputElement>, sanDomains: string) {
     setSanDomains(sanDomains);
   };
 
-  const commandBegin = ["sudo", "-u", "acme", "/usr/local/bin/acme.sh"];
-
-  const getDomainList = () => {
+  function getDomainList() {
       let result = ["-d", mainDomain];
       const sanDomainList = sanDomains.split(",");
       for (let domain of sanDomainList) {
@@ -34,19 +34,26 @@ export const AddDomainForm: React.FunctionComponent = () => {
       return result;
     };
 
-  const addCertificate = () => {
+  function addCertificate() {
     if (!mainDomain) {
       console.error("Main Domain Missing!");
       return;
     }
-    const domains = getDomainList()
+    const domains = getDomainList();
+    if (devMode) {
+      console.log(`Executing commands:
+        ${commandBegin.concat("--issue", domains, "--stateless", "--server", "letsencrypt")}
+        ${commandBegin.concat("--deploy", domains, "--deploy-hook", "haproxy", "DEPLOY_HAPROXY_PEM_PATH=/etc/haproxy/certs", "DEPLOY_HAPROXY_STATS_SOCKET=/var/lib/haproxy/stats", "DEPLOY_HAPROXY_HOT_UPDATE=yes")}`);
+        clearInput();
+        return;
+    }
     cockpit.spawn(commandBegin.concat("--issue", domains, "--stateless", "--server", "letsencrypt"), {superuser: "require"})
       .then(() => cockpit.spawn(commandBegin.concat("--deploy", domains, "--deploy-hook", "haproxy", "DEPLOY_HAPROXY_PEM_PATH=/etc/haproxy/certs", "DEPLOY_HAPROXY_STATS_SOCKET=/var/lib/haproxy/stats", "DEPLOY_HAPROXY_HOT_UPDATE=yes"), {superuser: "require"}))
       .then(clearInput)
       .catch(error => console.error(error));
   }
 
-  const clearInput = () => {
+  function clearInput() {
     setMainDomain("");
     setSanDomains("");
   }
